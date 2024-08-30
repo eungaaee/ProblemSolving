90ef3868330b4921c66077b82bff0d33723b5348
eungaaee <69461677+eungaaee@users.noreply.github.com>
2024-08-30 15:44:07 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    c += d;
+    b += c / 60;
+    c %= 60;
+    a += b / 60;
+    b %= 60;
+    a %= 24;
+    cout << a << ' ' << b << ' ' << c;
+    return 0;
+}
