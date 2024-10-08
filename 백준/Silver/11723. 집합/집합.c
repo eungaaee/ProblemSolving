382c5bdcb94a8a72c1d2060588678ebac7d4a602
eungaaee <69461677+eungaaee@users.noreply.github.com>
2024-10-09 01:40:35 +0900

    [Silver V] Title: 집합, Time: 744 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.c
+++ 백준/Silver/11723. 집합/집합.c
@@ -1,46 +1,38 @@
-#include <malloc.h>
 #include <stdio.h>
-int s = 0;
-void add(int op) {
-    s = s | (1 << op);
-}
-void rm(int op) {
-    s = s & ~(1 << op);
-}
-void check(int op) {
-    printf("%d\n", (s & (1 << op)) > 0);
-}
-void toggle(int op) {
-    s = s ^ (1 << op);
-}
-void all() {
-    s = -1;
-}
-void empty() {
-    s = 0;
-}
+
+int S; // (1 << 21) < INT_MAX
+
+void add(int x) { S |= (1 << x); }
+
+void rm(int x) { S &= ~(1 << x); }
+
+void check(int x) { printf("%d\n", (S & (1 << x)) > 0); }
+
+void toggle(int x) { S ^= (1 << x); }
+
+void all() { S = (1 << 21) - 1; } // 1000..0000 - 1 = 0111..1111
+
+void empty() { S = 0; }
+
 int main() {
-    int m, op2;
-    char op1[7];
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++) {
-        scanf("%s", op1);
-        if (*op1 == 'a' && *(op1+1) == 'd') {
-            scanf("%d", &op2);
-            add(op2);
-        } else if (*op1 == 'r') {
-            scanf("%d", &op2);
-            rm(op2);
-        } else if (*op1 == 'c') {
-            scanf("%d", &op2);
-            check(op2);
-        } else if (*op1 == 't') {
-            scanf("%d", &op2);
-            toggle(op2);
-        } else if (*op1 == 'a' && *(op1+1) == 'l') {
-            all();
-        } else {
-            empty();
+    int M;
+    scanf("%d", &M);
+
+    char op[7];
+    int x;
+    
+    while (M--) {
+        scanf("%s", op);
+
+        switch (op[1]) {
+            case 'd': scanf("%d", &x); add(x); break;
+            case 'e': scanf("%d", &x); rm(x); break;
+            case 'h': scanf("%d", &x); check(x); break;
+            case 'o': scanf("%d", &x); toggle(x); break;
+            case 'l': all(); break;
+            case 'm': empty(); break;
         }
     }
+
+    return 0;
 }
